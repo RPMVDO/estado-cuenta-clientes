451da3a02acd53ce159ed55641249a69ee01b36b
RPMVDO <contacto@rpmvdo.com.ar>
2025-04-11 12:38:42 -0300

    Update EstadoCuentaApp.jsx

--- src/EstadoCuentaApp.jsx
+++ src/EstadoCuentaApp.jsx
@@ -6,7 +6,7 @@ const Button = (props) => <button {...props} className="bg-blue-600 hover:bg-blu
 const Card = (props) => <div className="bg-white shadow rounded-lg border border-gray-200">{props.children}</div>;
 const CardContent = (props) => <div className="p-4">{props.children}</div>;
 
-const GOOGLE_SHEET_API_URL = "https://script.google.com/macros/s/AKfycbwU1a6secfi1getFByIQPMZ9TY-CdhuJ6vXaS3LX-zpZ05Mft-Sz0t8A0ljc5sfB3HUAw/exec"; // reemplazá con tu URL
+const GOOGLE_SHEET_API_URL = "https://script.google.com/macros/s/AKfycbxaFwjn0U_XvCfGBw-75u-LWW22r-8bmZPU1wDRRGJwtS63kM6H3u5dNUf7U9G8mP56AA/exec"; // reemplazá con tu URL
 
 export default function EstadoCuentaApp() {
   const [facturas, setFacturas] = useState([]);
