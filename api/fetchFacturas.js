ba1f204bbcc305c11eaa1f28e2c2f99f800e12aa
RPMVDO <contacto@rpmvdo.com.ar>
2025-04-15 18:05:28 -0300

    Update fetchFacturas.js

--- api/fetchFacturas.js
+++ api/fetchFacturas.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
   }
 
   try {
-    const response = await fetch("https://script.google.com/macros/s/AKfycbybaSy-ZVcNJjbmQtUhAQlj9OOCysx4AV2rvsAPzuAxHFHZFkwd5z0gxh7JOiBNDgo3KQ/exec");
+    const response = await fetch("https://script.google.com/macros/s/AKfycbyYz7Cqx-QNaYataaXpoBeZ3sLv8N1IDnAts14hLcxDOI1-zLBzjmagDS0BarLvgmClfQ/exec");
 
     if (!response.ok) {
       throw new Error("Error al obtener datos de Google Sheets");
