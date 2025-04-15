1a8b8caadfda542ba91b866ee2d71a6d5ff39bdd
RPMVDO <contacto@rpmvdo.com.ar>
2025-04-15 12:27:20 -0300

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import EstadoCuentaApp from "./EstadoCuentaApp";
-import "./style.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import EstadoCuentaERP from './EstadoCuentaApp';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <EstadoCuentaApp />
+    <EstadoCuentaERP />
   </React.StrictMode>
 );
